956a41bea37f4873026bec65a13981cf9c64facb
ktrianaa2 <ktrianaa2@uteq.edu.ec>
2023-12-19 16:04:45 -0500

    continuacion

--- app/src/main/java/com/example/contenedoresingsoftware/MainActivity3.java
+++ app/src/main/java/com/example/contenedoresingsoftware/MainActivity3.java
@@ -10,32 +10,42 @@ import com.example.contenedoresingsoftware.Adaptadores.AdaptadoresUsuarios;
 import com.example.contenedoresingsoftware.Models.Usuarios;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import WebServices.Asynchtask;
 import WebServices.WebService;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity implements Asynchtask {
 
+    ListView lstOpciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        ListView lstOpciones = (ListView) findViewById(R.id.listUsuario);
+        lstOpciones = (ListView) findViewById(R.id.listUsuario);
+
+        View header = getLayoutInflater().inflate(R.layout.ly_itemusuario, null);
+        lstOpciones.addHeaderView(header);
 
         Map<String, String> datos = new HashMap<String, String>();
-        WebService ws= new WebService("https://reqres.in/api/users",
-                datos,
-                MainActivity.this, MainActivity.this);
+        WebService ws = new WebService("https://reqres.in/api/users",
+                datos, MainActivity3.this, MainActivity3.this);
         ws.execute("GET");
+    }
 
+    @Override
+    public void processFinish(String result) throws JSONException {
         JSONObject JSONlista = new JSONObject(result);
-        JSONArray JSONlistaUsuarios= JSONlista.getJSONArray("data");
-        Object lstUsuarios = Usuarios.JsonObjectsBuild(JSONlistaUsuarios);
-        AdaptadoresUsuarios adapatorUsuario = new AdaptadoresUsuarios(this, lstUsuarios);
-        lstOpciones.setAdapter(adapatorUsuario);
+        JSONArray JSONlistaUsuarios = JSONlista.getJSONArray("data");
+        ArrayList<Usuarios> lstUsuarios = Usuarios.JsonObjectsBuild(JSONlistaUsuarios);
+
+        AdaptadoresUsuarios adaptadorUsuario = new AdaptadoresUsuarios(this, lstUsuarios);
+        lstOpciones.setAdapter(adaptadorUsuario);
     }
 }
